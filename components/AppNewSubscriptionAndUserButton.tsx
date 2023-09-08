0f22788663fff36e1fa0867292807231ebd35f87
Wojciech Marek <marek.wojciech@outlook.com>
2023-09-08 11:40:24 +0000

    feat: install radix-ui dropdown

--- components/AppNewSubscriptionAndUserButton.tsx
+++ components/AppNewSubscriptionAndUserButton.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ClerkLoading, useUser } from "@clerk/nextjs";
+import { DropdownMenu } from "@radix-ui/react-dropdown-menu";
 import { MoreVertical, Plus } from "lucide-react";
 import Image from "next/image";
 
@@ -23,9 +24,16 @@ export default function AppNewSubscriptionAndUserButton() {
       <div className="flex items-center justify-between w-full px-4 py-4">
         <Image src={user.imageUrl} alt="user profile image" width={40} height={40} className="rounded-full" />
         <p className="ml-2 text-sm font-semibold grow">{user.username}</p>
-        <button>
-          <MoreVertical />
-        </button>
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger asChild>
+            <button
+              className="rounded-full w-[35px] h-[35px] inline-flex items-center justify-center text-violet11 bg-white shadow-[0_2px_10px] shadow-blackA7 outline-none hover:bg-violet3 focus:shadow-[0_0_0_2px] focus:shadow-black"
+              aria-label="Customise options"
+            >
+              <MoreVertical />
+            </button>
+          </DropdownMenu.Trigger>
+        </DropdownMenu.Root>
       </div>
     </div>
   )
