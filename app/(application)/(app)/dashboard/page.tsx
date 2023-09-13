0f1093ae942e55f516a7764d7f13b6a83c719a92
Wojciech Marek <marek.wojciech@outlook.com>
2023-09-13 08:44:34 +0200

    feat: add stats bar logic

--- app/(application)/(app)/dashboard/page.tsx
+++ app/(application)/(app)/dashboard/page.tsx
@@ -1,9 +1,8 @@
 import AppContentHeader from "@/components/AppContentHeader";
+import DashboardMainStats from "@/components/DashboardMainStats";
 import SingleStats from "@/components/SingleStats";
 import { prisma } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
-import { SUBSCRIPTION_CURRENCY } from "@prisma/client";
-import { format } from "date-fns";
 
 const getDataForDashboard = async () => {
   const { userId } = auth();
@@ -39,21 +38,7 @@ export default async function DashboardPage() {
     <section className="h-full p-8">
       <AppContentHeader title="Dashboard" />
       <div className="grid mx-auto h-full max-w-7xl grid-cols-3 grid-rows-[80px,_1fr] gap-6 py-6">
-        <div className="col-span-3 row-start-1 row-end-2 rounded-xl bg-zinc-50 xl:col-span-2">
-          <div className="flex items-center justify-between h-full gap-6 mx-6">
-            <div>
-              <h1 className="text-xl font-semibold leading-5 tracking-wide">{format(new Date(), "LLLL")}</h1>
-              <p className="text-xs text-zinc-500">{format(new Date(), "yyyy")}</p>
-            </div>
-
-            <div className="flex gap-6">
-              <SingleStats sum={23.2} currency={SUBSCRIPTION_CURRENCY.PLN} description="Description here" />
-              <SingleStats sum={23.2} currency={SUBSCRIPTION_CURRENCY.PLN} description="Description here" />
-              <SingleStats sum={23.2} currency={SUBSCRIPTION_CURRENCY.PLN} description="Description here" />
-            </div>
-          </div>
-
-        </div>
+        <DashboardMainStats subscriptions={data ?? []} />
         <div className="col-span-3 row-start-2 row-end-3 p-6 rounded-xl bg-zinc-50 xl:col-span-2">stats</div>
         <div className="hidden row-span-2 rounded-xl bg-zinc-50 xl:flex">stats</div>
       </div>
